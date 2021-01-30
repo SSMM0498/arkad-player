048d380ce81c24064a55bf5511dd770158aa0cf0
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2021-01-30 15:13:12 +0000

    rename the timer file to actionTimelineScheduler

--- src/Player/actionTimelineScheduler.ts
+++ src/Player/actionTimelineScheduler.ts
@@ -5,7 +5,7 @@ import {
 } from '../../../recorder/src/Recorder/types';
 import { actionWithDelay } from './types';
 
-export class actionScheduler {
+export class actionTimelineScheduler {
     public timeOffset: number = 0;
 
     private actionsBuffer: actionWithDelay[];
