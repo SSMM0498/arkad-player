ba27ba0c7acb109c2a0a996d91e0770e33fab36e
Serigne Saliou Mbacké Mbaye <barsaliou1998@gmail.com>
2021-02-06 19:13:07 +0000

    start refactoring

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import NodeBuilder from './NodeBuilder/NodeBuilder'
-import Player from './Player/Player'
-import { actionScheduler } from './Player/Timer'
-
-export {
-    Player,
-    actionScheduler,
-    NodeBuilder
-}
